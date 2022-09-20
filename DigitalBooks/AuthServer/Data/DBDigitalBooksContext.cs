a253eb8f1b31bf7670432084c615205903d952b6
unknown <cogdotnet1692@iiht.tech>
2022-09-20 21:12:46 +0530

    Azure function added

--- DigitalBooks/AuthServer/Data/DBDigitalBooksContext.cs
+++ DigitalBooks/AuthServer/Data/DBDigitalBooksContext.cs
@@ -26,7 +26,7 @@ namespace AuthServer.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DBDigitalBooks;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:1sqlserver.database.windows.net,1433;Initial Catalog=DigitalBooks;Persist Security Info=False;User ID=nikhil;Password=sql@12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
---
+++ DigitalBooks/PaymentFunctionApp/PurchaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace PaymentFunctionApp
+{
+    public class PurchaseModel
+    {
+        public string EmailId { get; set; } = null!;
+        public int BookId { get; set; }
+        public string PaymentMode { get; set; } = null!;
+    }
+}
